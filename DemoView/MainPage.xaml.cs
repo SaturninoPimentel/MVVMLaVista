7a93c667f47a3c04644d4a718973f5e7a5a7f577
saturnino <isc_saturnino@outlook.com>
2015-05-26 04:06:41 -0500

    First commit

---
+++ DemoView/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DemoView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnBehaviors_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Behaviors));
+        }
+
+        private void BtnResources_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Resources));
+        }
+
+        private void BtnStyles_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(StylesPage));
+        }
+
+        private void BtnTransformations_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Transformations));
+        }
+
+        private void ellipse_Loaded(object sender, RoutedEventArgs e)
+        {
+            Motion.Begin(); 
+        }
+    }
+}
